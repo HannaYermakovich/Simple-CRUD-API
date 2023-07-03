cf8401af2d302f69f7395c11a4897be9717cb808
Hanna Yermakovich <hanna.yermakovich@yahoo.com>
2023-07-03 19:04:55 +0200

    changed readme file

--- src/server.ts
+++ src/server.ts
@@ -20,42 +20,42 @@ export const server = http.createServer();
 
 const db = new InMemoryDataBase();
 
-server.on('request', async (req: Request, res: Response) => {
-    const { isValidAPI, userId } = validateURL(req.url);
+server.on('request', async (req: Request, res: Response) => {    
+    try {
+        const { isValidAPI, userId } = validateURL(req.url);
 
-    if (isValidAPI) {
-
-        const METHODS: Methods = {
-            GET: () => {
-                if (userId) {
-                    getUserById(db, req, res);
-                } else {
-                    getAllUsers(db, req, res);
+        if (isValidAPI) {
+    
+            const METHODS: Methods = {
+                GET: () => {
+                    if (userId) {
+                        getUserById(db, req, res);
+                    } else {
+                        getAllUsers(db, req, res);
+                    }
+                },
+                POST: () => {
+                    postUser(db, req, res);
+                },
+                PUT: () => {
+                    putUser(db, req, res);
+                },
+                DELETE: () => {
+                    deleteUser(db, req, res);
                 }
-            },
-            POST: () => {
-                postUser(db, req, res);
-            },
-            PUT: () => {
-                putUser(db, req, res);
-            },
-            DELETE: () => {
-                deleteUser(db, req, res);
             }
+    
+            METHODS[req.method]();
+        } else {
+            res.writeHead(StatusCode.NOT_FOUND, { 'Content-Type': 'text/plain' });
+            res.end(ErrorMessages.ENDPOINT_NOT_EXIST);
         }
-
-        METHODS[req.method]();
-    } else {
-        res.writeHead(StatusCode.NOT_FOUND, { 'Content-Type': 'text/plain' });
-        res.end(ErrorMessages.ENDPOINT_NOT_EXIST);
+    } catch (error) {
+        console.error('Server error:', error);
+        res.writeHead(StatusCode.SERVICE_ERROR, { 'Content-Type': 'text/plain' });
+        res.end(ErrorMessages.SERVICE_ERROR);
     }
 })
-
-server.on('error', (error: Error, req: Request, res: Response) => {
-    console.error('Server error:', error);
-    res.writeHead(StatusCode.SERVICE_ERROR, { 'Content-Type': 'text/plain' });
-    res.end(ErrorMessages.SERVICE_ERROR);
-});
 
 server.listen(PORT, () => {
     console.log(`Listening a port ${PORT}`)
